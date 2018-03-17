49736175d89d15c847d9d2fa782373afd4d32f56
Thanapat Sorralump <thanapat423@gmail.com>
2018-03-17 19:46:45 +0700

    Initial Commit

---
+++ PhonemoQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhonemoQuiz
+//
+//  Created by Thanapat Sorralump on 17/3/2561 BE.
+//  Copyright © 2561 Thanapat Sorralump. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
